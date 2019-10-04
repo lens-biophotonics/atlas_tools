ccae6d8c02c03a7bd441bc96a1d5eb90f5745b24
Ludovico Silvestri <silvestri@lens.unifi.it>
2019-10-04 12:34:16 +0200

    Added conversion to nifty that keeps original 16bit scale

--- niftiutils.py
+++ niftiutils.py
@@ -38,7 +38,8 @@ def convertImage(in_path, out_path, reverse=False, bs=100, x_final=0.025, y_fina
                  y_pix=0.0104, z_pix=0.01, nl=110, gamma=0.3, mp=99.9, top=-1):
     import numpy as np
     import nibabel as nib
-    import os, logging
+    import os
+    import logging
     import skimage.external.tifffile as tiff
     from skimage.transform import rescale
 
@@ -46,7 +47,7 @@ def convertImage(in_path, out_path, reverse=False, bs=100, x_final=0.025, y_fina
     in_image = tiff.imread(in_path)
     logger.info('input image loaded')
 
-    temp = rescale(np.swapaxes(in_image, 0, 2), scale=((x_pix/x_final), (y_pix/y_final), (z_pix/z_final)),
+    temp = rescale(np.swapaxes(in_image, 0, 2), scale=((x_pix / x_final), (y_pix / y_final), (z_pix / z_final)),
                    multichannel=False, anti_aliasing=False, preserve_range=True)
     logger.info('image downscaled')
 
@@ -55,7 +56,7 @@ def convertImage(in_path, out_path, reverse=False, bs=100, x_final=0.025, y_fina
     temp = np.power(temp, gamma)
     if top == -1:
         top = np.percentile(temp, mp)
-    temp = (temp/top)*255
+    temp = (temp / top) * 255
     temp = temp.clip(max=255)
     if reverse:
         temp = np.flip(temp, 0)
@@ -85,8 +86,51 @@ def convertImage(in_path, out_path, reverse=False, bs=100, x_final=0.025, y_fina
     return top
 
 
-def merge(f_path, b_path, out_path, ms, t):
+def convertImage16(in_path, out_path, reverse=False, bs=100, x_final=0.025, y_final=0.025, z_final=0.025, x_pix=0.0104,
+                   y_pix=0.0104, z_pix=0.01):
+    import numpy as np
+    import nibabel as nib
+    import os
+    import logging
+    import skimage.external.tifffile as tiff
+    from skimage.transform import rescale
 
+    logger = logging.getLogger(__name__)
+    in_image = tiff.imread(in_path)
+    logger.info('input image loaded')
+
+    temp = rescale(np.swapaxes(in_image, 0, 2), scale=((x_pix / x_final), (y_pix / y_final), (z_pix / z_final)),
+                   multichannel=False, anti_aliasing=False, preserve_range=True)
+    logger.info('image downscaled')
+
+    if reverse:
+        temp = np.flip(temp, 0)
+        temp = np.flip(temp, 2)
+        temp = np.concatenate((temp, np.zeros((temp.shape[0], temp.shape[1], bs))), axis=2)
+
+    out_image = temp.astype('uint16')
+    logger.info('image processed')
+
+    nifti = nib.Nifti1Image(out_image, None)
+    nifti.header['pixdim'][1] = x_final
+    nifti.header['pixdim'][2] = y_final
+    nifti.header['pixdim'][3] = z_final
+    # 512 is the NIFTI code for unsigned short, see https://nifti.nimh.nih.gov/nifti-1/documentation/nifti1fields/
+    nifti.header['datatype'] = 512
+    nifti.header['bitpix'] = 16
+    # 2 is the NIFTI code for millimeters, see https://nifti.nimh.nih.gov/nifti-1/documentation/nifti1fields/
+    nifti.header['xyzt_units'] = 2
+
+    folder, file = os.path.split(in_path)
+    if out_path == 'NULL':
+        filename, ext = os.path.splitext(file)
+        out_path = os.path.join(folder, filename + "16bit.nii.gz")
+
+    nib.save(nifti, out_path)
+    logger.info('output image saved to %s', out_path)
+
+
+def merge(f_path, b_path, out_path, ms, t):
     import nibabel as nib
     import numpy as np
     import logging
@@ -101,21 +145,21 @@ def merge(f_path, b_path, out_path, ms, t):
     b_data = back.get_fdata()
     logger.info('back image loaded')
 
-    mid = int(float((front.header["dim"][3]))/2) + ms
+    mid = int(float((front.header["dim"][3])) / 2) + ms
     half = (int(float(t) / 2))
 
     out = np.zeros(f_data.shape).astype('uint8')
-    out[..., 0:(mid-half)] = f_data[..., 0:(mid-half)]
-    out[..., (mid+half):out.shape[2]] = b_data[..., (mid+half):out.shape[2]]
+    out[..., 0:(mid - half)] = f_data[..., 0:(mid - half)]
+    out[..., (mid + half):out.shape[2]] = b_data[..., (mid + half):out.shape[2]]
 
-    slab_front = np.zeros((out.shape[0], out.shape[1], 2*half))
-    slab_front[...] = f_data[..., (mid-half):(mid+half)]
-    slab_back = np.zeros((out.shape[0], out.shape[1], 2*half))
-    slab_back[...] = b_data[..., (mid-half):(mid+half)]
+    slab_front = np.zeros((out.shape[0], out.shape[1], 2 * half))
+    slab_front[...] = f_data[..., (mid - half):(mid + half)]
+    slab_back = np.zeros((out.shape[0], out.shape[1], 2 * half))
+    slab_back[...] = b_data[..., (mid - half):(mid + half)]
 
-    for i in range(2*half):
-        out[..., (mid-half)+i] = (slab_front[..., i] * (((2*half-1)-i)/(2*half-1))
-                                        + slab_back[..., i]*(i/(2*half-1))).astype('uint8')
+    for i in range(2 * half):
+        out[..., (mid - half) + i] = (slab_front[..., i] * (((2 * half - 1) - i) / (2 * half - 1))
+                                      + slab_back[..., i] * (i / (2 * half - 1))).astype('uint8')
 
     logger.info('images merged')
 
---
+++ prepare2sides16.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+b2f_dist = 2.68
+excess_border = 10
+
+
+def main():
+    from niftiutils import convertImage16
+    import logging
+    import coloredlogs
+    import os
+    import argparse
+    import skimage.external.tifffile as tiff
+
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(format='[%(funcName)s] - %(asctime)s - %(message)s', level=logging.INFO)
+    coloredlogs.install(level='DEBUG', logger=logger)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-f', '--front', help='front side path', metavar='PATH')
+    parser.add_argument('-b', '--back', help='back side path', metavar='PATH')
+    parser.add_argument('-o', '--output', help="output base path", metavar='PATH')
+    parser.add_argument('-x-final', type=float, default=0.025, help="final voxel size along x (in mm)")
+    parser.add_argument('-y-final', type=float, default=0.025, help="final voxel size along y (in mm)")
+    parser.add_argument('-z-final', type=float, default=0.025, help="final voxel size along z (in mm)")
+    parser.add_argument('-x-pix', type=float, default=0.0104, help="initial voxel size along x (in mm)")
+    parser.add_argument('-y-pix', type=float, default=0.0104, help="initial voxel size along y (in mm)")
+    parser.add_argument('-z-pix', type=float, default=0.01, help="initial voxel size along z (in mm)")
+    args = parser.parse_args()
+
+    front = tiff.TiffFile(args.front)
+    back = tiff.TiffFile(args.back)
+
+    logger.info('processing front image...')
+    black = int((len(back.pages) - b2f_dist / args.z_pix + excess_border) * (args.z_pix / args.z_final))
+    base, front_file = os.path.split(args.front)
+    name, ext = os.path.splitext(front_file)
+
+    # check if output folder exists
+    if not os.path.exists(args.output):
+        os.makedirs(args.output, 0o775)
+
+    fc_path = os.path.join(args.output, name + "16bit.nii.gz")
+    convertImage16(args.front, out_path=fc_path, reverse=True, bs=black, x_final=args.x_final, y_final=args.y_final,
+                   z_final=args.z_final, x_pix=args.x_pix, y_pix=args.y_pix, z_pix=args.z_pix)
+
+    logger.info('processing back image...')
+    base, back_file = os.path.split(args.back)
+    name, ext = os.path.splitext(back_file)
+    bc_path = os.path.join(args.output, name + "16bit.nii.gz")
+    convertImage16(args.back, out_path=bc_path, x_final=args.x_final, y_final=args.y_final, z_final=args.z_final,
+                   x_pix=args.x_pix, y_pix=args.y_pix, z_pix=args.z_pix)
+
+
+if __name__ == "__main__":
+    main()
