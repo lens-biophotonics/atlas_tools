ccec5f074833dcd321409dbc6a96abb2402c8643
Ludovico Silvestri <ludovicosilvestri@MBP-di-Ludovico.fritz.box>
2020-05-21 23:26:15 +0200

    added logs

--- composite.py
+++ composite.py
@@ -26,16 +26,19 @@ def main():
         if 'x_' in name:
             green_path = os.path.join(args.green, name)
             green = tifffile.imread(green_path)
+            logger.info('opened GREEN image %s', green_path)
             token = name[0:17]
             red_name = next((name2 for name2 in red_dir if token in name2), None)
             red_path = os.path.join(args.red, red_name)
             red = tifffile.imread(red_path)
+            logger.info('opened RED image %s', red_path)
             shape = green.shape
             merge = np.zeros((shape[0], 2, shape[1], shape[2])).astype(green.dtype)
             merge[:, 0, ...] = red
             merge[:, 1, ...] = green
             merge_path = os.path.join(args.output, name)
             tifffile.imwrite(merge_path, merge, imagej=True, photometric=True, metadata={'mode':'composite'})
+            logger.info('composite image saved to %s', merge_path)
 
 
 if __name__ == "__main__":
