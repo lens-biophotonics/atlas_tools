bb85bcb8ced453610afefb3fe0c3852a64a0a154
Ludovico Silvestri <silvestri@lens.unifi.it>
2020-05-22 16:39:39 +0200

    random_substack_generator: added a threshold on 95% percentile to avoid using black stacks

--- random_substack_generator.py
+++ random_substack_generator.py
@@ -9,6 +9,7 @@ def main():
     import skimage.external.tifffile as tiff
     from zetastitcher import VirtualFusedVolume
     import random
+    import numpy as np
 
     logger = logging.getLogger(__name__)
     logging.basicConfig(format='[%(funcName)s] - %(asctime)s - %(message)s', level=logging.INFO)
@@ -22,22 +23,26 @@ def main():
     parser.add_argument('-xs', '--xsize', type=int, default=480, help="x size of the substack (in voxels)")
     parser.add_argument('-ys', '--ysize', type=int, default=480, help="y size of the substack (in voxels)")
     parser.add_argument('-zs', '--zsize', type=int, default=480, help="z size of the substack (in voxels)")
+    parser.add_argument('-t', '--threshold', type=int, default=150, help="threshold of 95% percentile")
 
     args = parser.parse_args()
 
     vfv = VirtualFusedVolume(args.input)
     random.seed()
     limits = vfv.shape
+    n = 0
 
-    for n in range(args.nstacks):
+    while n < args.nstacks:
         logger.info('generating substack # %d' % n)
         x = random.randint(0, limits[2]-(args.xsize+1))
         y = random.randint(0, limits[1]-(args.ysize+1))
         z = random.randint(0, limits[0]-(args.zsize+1))
 
         temp = vfv[z:(z+args.zsize), y:(y+args.ysize), x:(x+args.xsize)]
-        path = os.path.join(args.output, args.suffix + '_%04d_%05d_%05d.tiff' % (z, y, x))
-        tiff.imsave(path, temp)
+        if np.percentile(temp, 95) > args.threshold:
+            path = os.path.join(args.output, args.suffix + '_%04d_%05d_%05d.tiff' % (z, y, x))
+            tiff.imsave(path, temp)
+            n = n+1
 
 
 if __name__ == "__main__":
