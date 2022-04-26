02b9ee6afee4396de541b7e5859b779a1e71d09e
Ludovico Silvestri <silvestri@lens.unifi.it>
2022-04-26 15:19:08 +0200

    niftiutils: correct call to InputFile

--- niftiutils.py
+++ niftiutils.py
@@ -259,7 +259,8 @@ def tif2nii(in_path, out_path, x_pix=0.025, y_pix=0.025, z_pix=0.025):
     from zetastitcher import InputFile
 
     logger = logging.getLogger(__name__)
-    image = InputFile(in_path)
+    imfile = InputFile(in_path)
+    image = imfile.whole()
     logger.info('input image loaded')
     out_image = np.swapaxes(image, 0, 2)
     nifti = nib.Nifti1Image(out_image, None)
